5e499ab3bf732a1ad05c80222279b1b4540b86fc
Bruce Krayenhoff <wbrucek@gmail.com>
2022-06-13 10:25:32 -0700

    Shortened Input to RPSLS game

--- lesson2_small_programs/rock_paper_scissors.js
+++ lesson2_small_programs/rock_paper_scissors.js
@@ -1,5 +1,6 @@
 const readline = require('readline-sync');
 const VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard'];
+const VALID_CHOICES_CODES = ['r', 'p', 'sc', 'sp', 'l'];
 let whatBeatsWhat = {};
 for (let ind = 0; ind < VALID_CHOICES.length; ind++) {
   whatBeatsWhat[VALID_CHOICES[ind]] =
@@ -25,14 +26,22 @@ function displayWinner(choice, computerChoice) {
   }
 }
 
-while (true) {
-  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-  let choice = readline.question();
-
-  while (!VALID_CHOICES.includes(choice)) {
-    prompt("That's not a valid choice");
-    choice = readline.question();
+function getValidChoice() {
+  while (true) {
+    prompt(`Choose one: ${VALID_CHOICES.join(', ')}, or use one of the corresponding codes: ${VALID_CHOICES_CODES.join(', ')}`);
+    let choice = readline.question();
+    if (VALID_CHOICES_CODES.includes(choice)) {
+      choice = VALID_CHOICES[VALID_CHOICES_CODES.indexOf(choice)];
+    }
+    if (VALID_CHOICES.includes(choice)) {
+      return choice;
+    }
+    prompt("That's not a valid choice, please try again");
   }
+}
+
+while (true) {
+  const choice = getValidChoice();
 
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
   let computerChoice = VALID_CHOICES[randomIndex];
