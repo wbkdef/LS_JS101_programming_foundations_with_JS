b6cc46ab3c4e6ea12cf44b135ab5c04cb08304c9
Bruce Krayenhoff <wbrucek@gmail.com>
2022-06-13 10:04:24 -0700

    Starting to modify rock paper scissors program

---
+++ js101_small_problems/string_and_text_processing.js
@@ -0,0 +1,143 @@
+
+console.log(`\n Uppercase Check - https://launchschool.com/exercises/59cd044d`);
+/**
+ * @param {String} str 
+ * @returns Whether all alphabetic characters are upper case
+ */
+function isUppercase(str) {
+  return str === str.toUpperCase();
+}
+console.log(isUppercase('t'));               // false
+console.log(isUppercase('T'));               // true
+console.log(isUppercase('Four Score'));      // false
+console.log(isUppercase('FOUR SCORE'));      // true
+console.log(isUppercase('4SCORE!'));         // true
+console.log(isUppercase(''));                // true
+
+
+console.log(`\n Delete Vowels - https://launchschool.com/exercises/8249329a`);
+
+function removeVowelsFromString(string) {
+  return string
+    .replace(/a/g, '')
+    .replace(/e/g, '')
+    .replace(/i/g, '')
+    .replace(/o/g, '')
+    .replace(/u/g, '')
+    .replace(/A/g, '')
+    .replace(/E/g, '')
+    .replace(/I/g, '')
+    .replace(/O/g, '')
+    .replace(/U/g, '');
+}
+
+/**
+ * Returns modified input string with vowels removed
+ * @param {Array<String>} string String to remove vowels from
+ * @returns "string" with vowels removed
+ */
+function removeVowels(strings) {
+  return strings.map(str => removeVowelsFromString(str));
+}
+console.log(removeVowels(['abcdefghijklmnopqrstuvwxyz']));         // ["bcdfghjklmnpqrstvwxyz"]
+console.log(removeVowels(['green', 'YELLOW', 'black', 'white']));  // ["grn", "YLLW", "blck", "wht"]
+console.log(removeVowels(['ABC', 'AEIOU', 'XYZ']));                // ["BC", "", "XYZ"]
+
+
+console.log(`\n Lettercase Counter - https://launchschool.com/exercises/6a9bb11e`);
+// Use a global regex to get all matches of each of the first 2 types
+// Use the length of the string minus the counts of the first two types to get the third
+/**
+ * @param {String} string
+ */
+function letterCaseCount(string) {
+  const lowerMatches = string.match(/[a-z]/g);
+  const nLower = lowerMatches ? lowerMatches.length : 0;
+  const upperMatches = string.match(/[A-Z]/g);
+  const nUpper = upperMatches ? upperMatches.length : 0;
+  return {
+    lowercase: nLower,
+    uppercase: nUpper,
+    neither: string.length - nLower - nUpper
+  };
+}
+
+console.log(letterCaseCount('abCdef 123'));  // { lowercase: 5, uppercase: 1, neither: 4 }
+console.log(letterCaseCount('AbCd +Ef'));    // { lowercase: 3, uppercase: 3, neither: 2 }
+console.log(letterCaseCount('123'));         // { lowercase: 0, uppercase: 0, neither: 3 }
+console.log(letterCaseCount(''));            // { lowercase: 0, uppercase: 0, neither: 0 }
+
+
+console.log(`\n Lettercase Counter, TRY 2 - https://launchschool.com/exercises/6a9bb11e`);
+// Use a global regex to get all matches of each of the 3 types
+// Return the lengths of the matches in an object
+/**
+ * @param {String} string
+ */
+function letterCaseCount2(string) {
+  const matchesLowercase = string.match(/[a-z]/g) || [];
+  const matchesUppercase = string.match(/[A-Z]/g) || [];
+  const matchesNeither = string.match(/[^a-z]/gi) || [];
+  return {
+    lowercase: matchesLowercase.length,
+    uppercase: matchesUppercase.length,
+    neither: matchesNeither.length,
+  };
+}
+
+console.log(letterCaseCount2('abCdef 123'));  // { lowercase: 5, uppercase: 1, neither: 4 }
+console.log(letterCaseCount2('AbCd +Ef'));    // { lowercase: 3, uppercase: 3, neither: 2 }
+console.log(letterCaseCount2('123'));         // { lowercase: 0, uppercase: 0, neither: 3 }
+console.log(letterCaseCount2(''));            // { lowercase: 0, uppercase: 0, neither: 0 }
+
+
+console.log(`\n Capitalize Words - https://launchschool.com/exercises/1fd9c225`);
+// Split on spaces
+// Capitalize the first letter of each word, lower case the rest
+// Join into 1 string again and return
+
+/**
+ * Capitalizes the first letter of each word.
+ * @param {String} string 
+ */
+function wordCap(string) {
+  return string
+    .split(' ')
+    .map(word => word[0].toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
+}
+console.log(wordCap('four score and seven'));       // "Four Score And Seven"
+console.log(wordCap('the javaScript language'));    // "The Javascript Language"
+console.log(wordCap('this is a "quoted" word'));    // 'This Is A "quoted" Word'
+
+
+console.log(`\n Swap Case - https://launchschool.com/exercises/7d7dbbbe`);
+// Go through character by character
+// If lower case (regex test), change to uppercase and save to array
+// ElseIf upper case (regex test), change to lowercase and save to array
+// Else save to array unmodified
+// Join characters into a string and return
+/**
+ * Returns the string with every lowercase letter changed to uppercase
+ * and every uppercase letter changed to lowercase. Leaves
+ * all other characters unchanged.
+ * @param {String} string String to modify and return.
+ */
+function swapCase(string) {
+  const res = string
+    .split('')
+    .map(char => {
+      if (/[A-Z]/.test(char)) {
+        return char.toLowerCase();
+      } else if (/[a-z]/.test(char)) {
+        return char.toUpperCase();
+      } else {
+        return char;
+      }
+    })
+    .join('');
+  return res;
+}
+
+console.log(swapCase('CamelCase'));              // "cAMELcASE"
+console.log(swapCase('Tonight on XYZ-TV'));      // "tONIGHT ON xyz-tv"
---
+++ lesson2_small_programs/rock_paper_scissors.js
@@ -0,0 +1,52 @@
+const readline = require('readline-sync');
+const VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard'];
+let whatBeatsWhat = {};
+for (let ind = 0; ind < VALID_CHOICES.length; ind++) {
+  whatBeatsWhat[VALID_CHOICES[ind]] =
+    [VALID_CHOICES[(ind + 2) % 5], VALID_CHOICES[(ind + 4) % 5]];
+}
+// console.log(whatBeatsWhat);
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function displayWinner(choice, computerChoice) {
+  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+
+  if ((choice === 'rock' && computerChoice === 'scissors') ||
+      (choice === 'paper' && computerChoice === 'rock') ||
+      (choice === 'scissors' && computerChoice === 'paper')) {
+    prompt('You win!');
+  } else if ((choice === 'rock' && computerChoice === 'paper') ||
+             (choice === 'paper' && computerChoice === 'scissors') ||
+             (choice === 'scissors' && computerChoice === 'rock')) {
+    prompt('Computer wins!');
+  } else {
+    prompt("It's a tie!");
+  }
+}
+
+while (true) {
+  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+  let choice = readline.question();
+
+  while (!VALID_CHOICES.includes(choice)) {
+    prompt("That's not a valid choice");
+    choice = readline.question();
+  }
+
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[randomIndex];
+
+  displayWinner(choice, computerChoice);
+
+  prompt('Do you want to play again (y/n)?');
+  let answer = readline.question().toLowerCase();
+  while (answer[0] !== 'n' && answer[0] !== 'y') {
+    prompt('Please enter "y" or "n".');
+    answer = readline.question().toLowerCase();
+  }
+
+  if (answer[0] !== 'y') break;
+}
